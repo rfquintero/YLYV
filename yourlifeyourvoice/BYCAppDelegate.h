7472a05c1c6e7f8e76f96ecc313b14d68e261023
Ruben Quintero <Ruben@Rubens-MacBook-Pro.local>
2014-02-20 11:38:19 -0600

    Initial Commit

---
+++ yourlifeyourvoice/BYCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BYCAppDelegate.h
+//  yourlifeyourvoice
+//
+//  Created by Ruben Quintero on 2/20/14.
+//  Copyright (c) 2014 Banyan Communications. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BYCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ yourlifeyourvoice/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  yourlifeyourvoice
+//
+//  Created by Ruben Quintero on 2/20/14.
+//  Copyright (c) 2014 Banyan Communications. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BYCAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BYCAppDelegate class]));
+    }
+}
