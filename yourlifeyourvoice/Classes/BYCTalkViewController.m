09866573b7f5ab2fefca28baebdeda362a7f1ba4
Ruben Quintero <rfquintero@gmail.com>
2014-03-28 14:29:31 -0500

    update time checks
    - ruben

--- yourlifeyourvoice/Classes/BYCTalkViewController.m
+++ yourlifeyourvoice/Classes/BYCTalkViewController.m
@@ -43,27 +43,23 @@
 
 -(void)chatSelected {
     [self trackEvent:BYCTrackingContact action:@"chat" label:@"talk" value:nil];
-    if([[NSDate date] isOnDays:@[@(2),@(3),@(4),@(5)] startHour:19 startMinute:30 endHour:23 endMinute:59]) {
+    if([[NSDate date] isOnDays:@[@(2),@(3),@(4),@(5)] startHour:19 startMinute:00 endHour:23 endMinute:59]) {
         [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"http://www.lifeline-chat.org/SightMaxAgentInterface/PreChatSurvey.aspx?accountID=5&siteID=8&queueID=17"]];
     } else {
-        [self showError:@"Online Chat Unavailable" message:@"Chat will be available on Mondays - Thursdays from 7:30PM - 12:00 AM CDT. Please call or email."];
+        [self showError:@"Online Chat Unavailable" message:@"Chat will be available on Mondays - Thursdays from 7:00PM - 12:00 AM CDT. Please call or email."];
     }
 }
 
 -(void)textSelected {
     [self trackEvent:BYCTrackingContact action:@"text" label:@"talk" value:nil];
-    if([[NSDate date] isOnDays:@[@(2),@(4)] startHour:19 startMinute:00 endHour:23 endMinute:59]) {
-        if([MFMessageComposeViewController canSendText]) {
-            MFMessageComposeViewController *vc = [[MFMessageComposeViewController alloc] init];
-            [vc setRecipients:@[@"20121"]];
-            [vc setBody:@"VOICE"];
-            [vc setMessageComposeDelegate:self];
-            [self.navigationController presentViewController:vc animated:YES completion:^{}];
-        } else {
-            [self showError:@"Cannot send text" message:@"Messaging is not available. Please check your settings."];
-        }
+    if([MFMessageComposeViewController canSendText]) {
+        MFMessageComposeViewController *vc = [[MFMessageComposeViewController alloc] init];
+        [vc setRecipients:@[@"20121"]];
+        [vc setBody:@"VOICE"];
+        [vc setMessageComposeDelegate:self];
+        [self.navigationController presentViewController:vc animated:YES completion:^{}];
     } else {
-        [self showError:@"Text Unavailable" message:@"Text will be available on Monday and Wednesday from 7:00PM - 12:00 AM CDT. Please call or email."];
+        [self showError:@"Cannot send text" message:@"Messaging is not available. Please check your settings."];
     }
 }
 
